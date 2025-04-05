9107d391fbd656af32c89972934e8fa7d2d903fc
MidnightOracle <138881193+MidnightOracle@users.noreply.github.com>
2025-04-05 11:51:22 +0530

    Delete fitness-site-backup directory

--- fitness-site-backup/src/src/components/providers/client-providers.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { ToastProvider } from "@/components/ui/use-toast";
-
-export function ClientProviders({ children }: { children: React.ReactNode }) {
-  return <ToastProvider>{children}</ToastProvider>;
-} 
